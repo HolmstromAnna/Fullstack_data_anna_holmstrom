2f8f3d2edbf5f454c5bcc2f58afd5721a50e7b8e
Anna <annaholmstrom@live.com>
2024-09-18 09:06:16 +0200

    Started with exercise 3 chatbot

---
+++ exercises/3_chatbot/dashboard.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from components.bot import Bot
+
+def initialize_session_state():
+    """Initiliaze session state variables."""
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    if "bot" not in st.session_state:
+        st.session_state.bot = Bot()
+
