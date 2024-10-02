6014c770a319a54ad21380f9bcb591675fa577e5
Anna <annaholmstrom@live.com>
2024-10-02 10:00:59 +0200

    Track new frontend, utils, and main scripts

---
+++ lab/dashboard.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from frontend.kpi import ContentKPI
+from frontend.graphs import ViewsTrend
+
+content_kpi = ContentKPI()
+views_trend = ViewsTrend()
+
+def layout():
+    st.markdown("# The data driven youtuber")
+    st.markdown("Den här dashboarden syftar till att utforska datan i Kokchuns youtubekanal")
+
+    content_kpi.display_content()
+    views_trend.display_plot()
+
+if __name__ == "__main__":
+    layout()
+
---
+++ lab/frontend/kpi.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from utils.query_database import QueryDatabase
+
+class ContentKPI:
+    def __init__(self) -> None:
+        self.content = QueryDatabase("SELECT * FROM marts.content_view_time;")
+
+    def display_content(self):
+        df = self.content.df
+
+        st.markdown("## KPIer för videor")
+        st.markdown("Nedan visas KPIer för totalt antal")
+        
+        kpis = {
+            "videor": len(df),
+            "visade timmar": df["Visningstid_timmar"].sum(),
+            "prenumeranter": df["Prenumeranter"].sum(),
+            "exponeringar": df["Exponeringar"].sum()
+        }    
+        
+        for col, kpi in zip(st.columns(len(kpis)), kpis):
+            with col:
+                st.metric(kpi, round(kpis[kpi]))
+        
+        
+        #st.dataframe(df)
