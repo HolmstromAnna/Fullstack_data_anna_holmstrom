a4d42aac81f16c567b29a2cb188f8184e3e2cf71
Anna <annaholmstrom@live.com>
2024-10-03 12:35:27 +0200

    Integrated gender and age into the dashboard display

--- lab/dashboard.py
+++ lab/dashboard.py
@@ -1,15 +1,22 @@
 import streamlit as st
-from frontend.kpi import ContentKPI
+from frontend.kpi import Content
+from frontend.kpi import Gender
+from frontend.kpi import Age
 from frontend.graphs import ViewsTrend
 
-content_kpi = ContentKPI()
+
+content = Content()
 views_trend = ViewsTrend()
+gender = Gender()
+age = Age()
 
 def layout():
     st.markdown("# The data driven youtuber")
     st.markdown("Den här dashboarden syftar till att utforska datan i Kokchuns youtubekanal")
 
-    content_kpi.display_content()
+    content.display_content()
+    gender.display_gender()
+    age.display_age()
     views_trend.display_plot()
 
 if __name__ == "__main__":
