bbbe0b1920ad7b7bd196aca82281226b8d599ac5
Anna <annaholmstrom@live.com>
2024-10-07 10:01:00 +0200

    Add marts table traffic source and subplots

--- lab/backend/sql/Scripts/marts_content.sql
+++ lab/backend/sql/Scripts/marts_content.sql
@@ -2,10 +2,10 @@ DESC;
 
 SELECT * FROM information_schema.schemata WHERE catalog_name = 'youtube_data';
 
-
+-- skapa marts schema
 CREATE SCHEMA IF NOT EXISTS marts;
 
-
+-- skapa tabell med innehållets visningstid
 CREATE TABLE IF NOT EXISTS marts.content_view_time AS
 (
 SELECT
@@ -23,10 +23,10 @@ ORDER BY
 );
 
 
-
 SELECT * FROM marts.content_view_time;
 
 
+-- skapa tabell visningar per datum 
 CREATE TABLE IF NOT EXISTS marts.views_per_date AS
 (
 SELECT
@@ -34,38 +34,67 @@ SELECT
 	Datum) AS Datum,
 	Visningar
 FROM
-	innehall.totalt
-);
+	innehall.totalt);
 
-	
+SELECT * FROM marts.views_per_date;
+
+
+
+-- visa alla tabeller där table_schema är marts	
 SELECT
 	*
 FROM
 	information_schema.tables
 WHERE
 	table_schema = 'marts';
-	
-SELECT * FROM marts.views_per_date;
 
 
-
+-- skapa tabell med tittarnas kön
 CREATE TABLE IF NOT EXISTS marts.viewers_gender AS
 (
 SELECT 
 	"Tittarnas kön", 
 	"Visningar (%)"
 FROM
-	tittare.tabelldata_alder
-);
+	tittare.tabelldata_alder);
 
 
+-- skapa tabell med tittarnas ålder
 CREATE TABLE IF NOT EXISTS marts.viewers_age AS
 (
 SELECT
 	"Tittarnas ålder",
 	"Visningar (%)"
 FROM
-	tittare.tabelldata_kon
-);
+	tittare.tabelldata_kon);
 
 SELECT * FROM marts.viewers_age;
+
+
+
+SELECT * FROM trafikkalla.tabelldata;
+
+-- skapa tabell med trafikkällor
+CREATE TABLE IF NOT EXISTS marts.traffic_source AS
+(
+SELECT 
+    Trafikkälla,
+    Visningar,
+    "Visningstid (timmar)",
+    "Genomsnittlig visningslängd"
+FROM
+    trafikkalla.tabelldata);
+   
+
+-- avrunda timmar i Visningstid (timmar)
+SELECT * FROM marts.traffic_source
+
+UPDATE marts.traffic_source 
+SET "Visningstid (timmar)" = ROUND("Visningstid (timmar)");
+
+-- ta bort kolumn Genomsnittlig visningslängd
+ALTER TABLE marts.traffic_source 
+DROP COLUMN "Genomsnittlig visningslängd";
+
+
+
